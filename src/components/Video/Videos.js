f885eb2e830d6454d97c2f1d13d09a971aeccb53
yash <yashsuthar010@gmail.com>
2022-04-14 17:32:25 +0530

    rahul made me to do this

--- src/components/Video/Videos.js
+++ src/components/Video/Videos.js
@@ -1,11 +1,5 @@
 import React from "react";
 import "./style.css";
-import img3 from "../../images/img3.jpg";
-import img2 from "../../images/img2.jpg";
-import img4 from "../../images/img4.jpg";
-import img1 from "../../images/img1.jpg";
-import { FaPlayCircle } from "react-icons/fa";
-import vid1 from "../../videos/vid1.mp4";
 
 const Videos = () => {
   return (
@@ -15,9 +9,17 @@ const Videos = () => {
           Explore before you visit
         </h1>
         <div className="video-player">
-          <video width="100%" controls autoPlay className="vid">
-            <source src={vid1} type="video/mp4" />
-          </video>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/tQ0yjYUFKAE"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+            style={{ height: "100%", width: "100%", borderRadius: "30px" }}
+          ></iframe>
+          >
         </div>
       </div>
     </>
