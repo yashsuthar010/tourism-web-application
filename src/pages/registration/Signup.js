f4b572e428b056880d310a796372df9497189bf1
yash <yashsuthar010@gmail.com>
2022-04-15 12:40:55 +0530

    added routing'

---
+++ src/pages/registration/Signup.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Signup = () => {
+  return (
+    <>
+      <div>
+        <h1> you are succesfully signed up</h1>
+      </div>
+    </>
+  );
+};
+
+export default Signup;
